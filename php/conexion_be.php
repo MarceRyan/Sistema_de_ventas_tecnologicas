d4b630709e4b207c9622258284ee889b29c2b5e4
Marcelo Bryan Morales Rimac <marcelo999546479@gmail.com>
2024-04-30 11:03:45 -0500

    Modifique la pagina inicio

---
+++ php/conexion_be.php
@@ -0,0 +1,10 @@
+<?php
+    $conexion = mysqli_connect("localhost", "root", "", "login_register_bd");
+
+    /*if($conexion){
+        echo 'Conectado exitosamente a la base de datos';
+    }else{
+        echo 'No se ha conectado  a la base de datos';
+    }*/
+    
+?>
